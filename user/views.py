0f62f01cd92b94e4c6cdf2cca72489c1a6485142
KIM JUN SIK <silasoni94@gmail.com>
2022-10-14 17:27:51 +0900

    오류 발생하였으나 우선 제출하겠습니다

---
+++ user/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+
+
+
+def sign_up_view(request):
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        password2 = request.POST.get('password2', None)
+        bio = request.POST.get('bio', None)
+
+        if password != password2:
+            return render(request, 'user/signup.html')
+        else:
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html') # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
+            else:
+                UserModel.objects.create_user(username=username, password=password, bio=bio)
+                return redirect('/sign-in') # 회원가입이 완료되었으므로 로그인 페이지로 이동
+
+
+
+def sign_in_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = auth.authenticate(request, username=username, password=password)  # 사용자 불러오기
+        if me is not None:  # 저장된 사용자의 패스워드와 입력받은 패스워드 비교
+            auth.login(request, me)
+            return HttpResponse("로그인 성공")
+        else:
+            return redirect('/sign-in')  # 로그인 실패
+    elif request.method == 'GET':
+        return render(request, 'user/signin.html')
+
+
+
+@login_required
+def logout(request):
+    auth.logout(request) # 인증 되어있는 정보를 없애기
+    return redirect("/")
+
+
+
+@login_required
+def user_view(request):
+    if request.method == 'GET':
+        # 사용자를 불러오기, exclude와 request.user.username 를 사용해서 '로그인 한 사용자'를 제외하기
+        user_list = UserModel.objects.all().exclude(username=request.user.username)
+        return render(request, 'user/user_list.html', {'user_list': user_list})
